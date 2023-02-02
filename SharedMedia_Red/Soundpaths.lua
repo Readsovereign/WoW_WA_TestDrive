23c3e111d4c696ca43d3e036ac6fd80007a84e64
Readsovereign <47609848+Readsovereign@users.noreply.github.com>
2023-02-02 08:43:38 -0500

    Add files via upload

---
+++ SharedMedia_Red/Soundpaths.lua
@@ -0,0 +1,22 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cFFFF0000HTTYD|r", [[Interface\Addons\SharedMedia_Causese\sound\HTTYD.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
